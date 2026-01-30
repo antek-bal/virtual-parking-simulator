c2637fb1046be63ee7da3c92f8bc8ad99fcf7c2d
S1N0X <antekbal06@gmail.com>
2026-01-30 20:43:14 +0100

    Created endpoints and updated parking manager class

--- src/app/main.py
+++ src/app/main.py
@@ -1,22 +1,63 @@
 from fastapi import FastAPI, HTTPException
+from src.app.schemas import EntryRequest, UpdateFloorRequest
 from src.app.services.parking_manager import ParkingManager
 from src.app.services.pricing import PriceCalculator
 from src.app.services.validator import VehicleValidator
 
-prices = {0:6, 1:5, 2:4, 3:3, 4:2}
+prices = {0: 6, 1: 5, 2: 4, 3: 3, 4: 2}
 calculator = PriceCalculator(prices)
-validator = VehicleValidator([["B", "C", "D", "E", "F", "G", "K", "L", "N", "O", "P", "R", "S", "T", "W", "Z"], ["H", "U"]])
+validator = VehicleValidator(["B", "C", "D", "E", "F", "G", "K", "L", "N", "O", "P", "R", "S", "T", "W", "Z"],
+                             ["H", "U"])
 manager = ParkingManager(calculator, validator)
 app = FastAPI(title="Virtual Parking Simulator")
+
 
 @app.get("/")
 def read_root():
     return {"message": "Parking Simulator is online"}
 
-@app.post("/entry")
-def register_vehicle_entry(country: str, registration_no: str, floor: int):
+
+@app.post("/entry", status_code=201)
+def register_vehicle_entry(entry: EntryRequest):
     try:
-        success = manager.register_entry(country, registration_no, floor)
+        success = manager.register_entry(entry.country, entry.registration_no, entry.floor)
+        return {"status": success, "registration_no": entry.registration_no}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.patch("/entry/{registration_no}")
+def update_floor(registration_no: str, update_data: UpdateFloorRequest):
+    if registration_no not in manager.active_parkings:
+        raise HTTPException(status_code=404, detail="Vehicle not found")
+
+    manager.active_parkings[registration_no]["floor"] = update_data.new_floor
+    return {"status": True, "registration_no": registration_no, "new_floor": update_data.new_floor}
+
+
+@app.delete("/entry/{registration_no}")
+def register_vehicle_exit(registration_no: str):
+    try:
+        success = manager.register_exit(registration_no)
         return {"status": success, "registration_no": registration_no}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get("/payment/{registration_no}")
+def get_payment(registration_no: str):
+    try:
+        data = manager.get_payment_info(registration_no)
+        return data
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get('/vehicles')
+def get_list_of_vehicles():
+    return manager.active_parkings
+
+
+@app.get('/entry/history')
+def get_history():
+    return manager.history
--- src/tests/unit/test_parking_manager.py
+++ src/tests/unit/test_parking_manager.py
@@ -53,3 +53,30 @@ class TestParkingManager:
         assert parking_manager.register_exit("GD5P227") == True
         assert len(parking_manager.active_parkings) == 0
 
+    def test_saving_to_history(self, parking_manager, mocker):
+        entry_time = datetime(2026, 1, 29, 10, 00, 00)
+
+        mock_datetime = mocker.patch('src.app.services.parking_manager.datetime')
+        mock_datetime.now.return_value = entry_time
+
+        parking_manager.register_entry("PL", "GD5P227", 3)
+
+        exit_time = datetime(2026, 1, 29, 11, 30, 00)
+
+        mock_datetime.now.return_value = exit_time
+
+        parking_manager.register_exit("GD5P227")
+
+        assert parking_manager.history["GD5P227"][0]["entry_time"] == entry_time
+        assert parking_manager.history["GD5P227"][0]["exit_time"] == exit_time
+        assert parking_manager.history["GD5P227"][0]["floor"] == 3
+        assert parking_manager.history["GD5P227"][0]["fee"] == 3.0
+
+    def test_multiple_parkings_in_history(self, parking_manager):
+        parking_manager.register_entry("PL", "GD5P227", 0)
+        parking_manager.register_exit("GD5P227")
+
+        parking_manager.register_entry("PL", "GD5P227", 1)
+        parking_manager.register_exit("GD5P227")
+
+        assert len(parking_manager.history["GD5P227"]) == 2
